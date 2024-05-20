8be258897ce2e7cae0062e76d78737f66dbb2233
john mbugua <johnymbugua1997@gmail.com>
2024-05-20 16:29:45 +0300

    context api and reducer function

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext } from "react";
+import { CartContext } from "../context_shop/shop_cart_context";
+
+export default function Cart() {
+  // item value from the context
+  const { items, updateToCartItem } = useContext(CartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -20,11 +26,11 @@ export default function Cart({ items, onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateToCartItem(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateToCartItem(item.id, 1)}>
                     +
                   </button>
                 </div>
@@ -39,3 +45,56 @@ export default function Cart({ items, onUpdateItemQuantity }) {
     </div>
   );
 }
+
+// the  use of consumer for accessing the context value
+
+// export default function Cart({ onUpdateItemQuantity }) {
+//   return (
+//     <CartContext.Consumer>
+//       {(crtItems) => {
+//         const totalPrice = crtItems.items.reduce(
+//           (acc, item) => acc + item.price * item.quantity,
+//           0
+//         );
+//         const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+//         return (
+//           <div id="cart">
+//             {crtItems.items.length === 0 && <p>No items in cart!</p>}
+//             {crtItems.items.length > 0 && (
+//               <ul id="cart-items">
+//                 {crtItems.items.map((item) => {
+//                   const formattedPrice = `$${item.price.toFixed(2)}`;
+
+//                   return (
+//                     <li key={item.id}>
+//                       <div>
+//                         <span>{item.name}</span>
+//                         <span> ({formattedPrice})</span>
+//                       </div>
+//                       <div className="cart-item-actions">
+//                         <button
+//                           onClick={() => onUpdateItemQuantity(item.id, -1)}
+//                         >
+//                           -
+//                         </button>
+//                         <span>{item.quantity}</span>
+//                         <button
+//                           onClick={() => onUpdateItemQuantity(item.id, 1)}
+//                         >
+//                           +
+//                         </button>
+//                       </div>
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+//             )}
+//             <p id="cart-total-price">
+//               Cart Total: <strong>{formattedTotalPrice}</strong>
+//             </p>
+//           </div>
+//         );
+//       }}
+//     </CartContext.Consumer>
+//   );
+// }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,26 @@
+import { useContext } from "react";
+import { CartContext } from "../context_shop/shop_cart_context";
+
 export default function Product({
   id,
   image,
   title,
   price,
   description,
-  onAddToCart,
+  // onAddToCart,
 }) {
+  const { addToCartItem } = useContext(CartContext);
   return (
     <article className="product">
       <img src={image} alt={title} />
       <div className="product-content">
         <div>
           <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
+          <p className="product-price">${price}</p>
           <p>{description}</p>
         </div>
-        <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+        <p className="product-actions">
+          <button onClick={() => addToCartItem(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
